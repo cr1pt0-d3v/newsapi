39fb0d721168d28ae3092cfdbb0c748b6d7dac53
costel-dev <costelcuconoiu@gmail.com>
2020-11-02 23:23:50 +0100

    material-ui added, rename App.js with Blog.js and added folder and file components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Blog from "./Blog";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Blog />
   </React.StrictMode>,
   document.getElementById('root')
 );
